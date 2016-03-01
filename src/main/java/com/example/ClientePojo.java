670aa3add3affe1279bd7bab46cb3068f70dd099
PCM1509 <PCM1509>
2016-03-01 18:42:12 +0100

    Segundo commit

---
+++ src/main/java/com/example/ClientePojo.java
@@ -0,0 +1,49 @@
+package com.example;
+
+/**
+ * Created by PCM1509 on 01/03/2016.
+ */
+public class ClientePojo {
+
+    private long id;
+
+    private String name;
+    private String email;
+    private String address;
+
+    public ClientePojo() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
